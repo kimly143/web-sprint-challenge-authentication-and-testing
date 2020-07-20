bb1ee43b664ed02729ac1a82bb256f853fd94686
Kim Ly Buck <kim.3993@gmail.com>
2020-07-19 19:26:10 -0500

    Update auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,25 +4,39 @@ const userDb = require('../database/userDb');
 const router = require('express').Router();
 
 router.post('/register', async (req, res) => {
-  if (!req.body.username || !req.body.password){
+	if (!req.body.username || !req.body.password) {
 		return res.status(422).json({ error: 'username and password required to register!' });
-  }
-  try {
-    const newUser = {
-      username: req.body.username,
-      password: bcrypt.hashSync(req.body.password, 8),
-    };
-    const user = await userDb.createUser(newUser);
-    return res.status(201).json(user)
-  } catch (ex) {
-    console.error(ex);
-    return res.status(500).json({ error: 'failed to register new user' });
-  }
+	}
+	try {
+		const newUser = {
+			username: req.body.username,
+			password: bcrypt.hashSync(req.body.password, 8)
+		};
+		const user = await userDb.createUser(newUser);
+		return res.status(201).json(user);
+	} catch (ex) {
+		console.error(ex);
+		return res.status(500).json({ error: 'failed to register new user' });
+	}
 });
 
 router.post('/login', async (req, res) => {
-  // implement login
-  res.end()
+	try {
+    const user = await userDb.getUserByUsername(req.body.username);
+    
+		if (!user) {
+			return res.status(422).json({ error: 'failed to login' });
+		}
+		if (!bcrypt.compareSync(req.body.password, user.password)) {
+			return res.status(422).json({ error: 'failed to login' });
+		}
+    req.session.userId = user.id;
+    res.status(200).end();
+    
+	} catch (ex) {
+		console.error(ex);
+		return res.status(422).json({ error: 'failed to login' });
+	}
 });
 
 module.exports = router;
